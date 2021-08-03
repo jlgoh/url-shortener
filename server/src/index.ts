554b050049b08d4b1e27d4024ed8405fe235b303
jlgoh <gohxjl@gmail.com>
2021-08-04 01:04:35 +0800

    fix-9: heroku deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ require('./routes/shorten')(app, dbPool);
 // Routing for deployment
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets like our main.js or css file
-    app.use(express.static('../build'));
+    app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
     // Express will serve up the index.html file if it doesn't recognize the route
     app.get('*', (req, res) => {
