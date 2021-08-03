44d634bc85d80be270550d32d7fc4a8b08da9b0f
jlgoh <gohxjl@gmail.com>
2021-08-04 00:22:45 +0800

    fix-4: heroku deploy

--- server/src/queries/index.ts
+++ server/src/queries/index.ts
@@ -1,9 +1,21 @@
 const { Pool } = require('pg');
 
-module.exports = new Pool({
+// local db config
+const localDbConfig = {
     user: 'postgres',
     host: 'localhost',
     database: 'url-shortener',
     password: 'toor',
     port: 5432,
-});
+};
+
+// prod db config
+const prodDbConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PW,
+    port: process.env.DB_PORT,
+};
+
+module.exports = new Pool(process.env.NODE_ENV === 'production' ? prodDbConfig : localDbConfig);
